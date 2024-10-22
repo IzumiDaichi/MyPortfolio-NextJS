d4920048544d792e939bbe3d269dacc246d9c070
Izumi <izumidaichi2016@gmail.com>
2024-10-22 13:49:25 +0800

    Update
    -updated Submitted form
    -updated profile

--- src/components/aboutComponents/ProfileCard.jsx
+++ src/components/aboutComponents/ProfileCard.jsx
@@ -1,79 +1,85 @@
-import {React, useState } from "react";
+import { React, useState } from "react";
 import Image from "next/image";
 import ProfileImg from "@/Images/Profile.png";
 import ProfileImg2 from "@/Images/Profile2.png";
 import Socials from "../ui/Socials2";
 import { fadeIn } from "../../lib/framer";
 import { motion } from "framer-motion";
+
 const ProfileCard = () => {
+  // Define state for the image, text content, and background color
+  const [profile, setProfile] = useState({
+    img: ProfileImg, // Default image
+    name: "Jake Rupisan",
+    role: "TRAINING FOR WEB DEVELOPMENT",
+    description: "Student",
+  });
 
-    // Define state for the image and text content
-    const [profile, setProfile] = useState({
-      img: ProfileImg, // Default image
-      name: "Jake Rupisan",
-      role: "TRAINING FOR UI & UX, FRONTEND",
-      description: "Student",
-    });
-  
-    // Define alternative profile content
-    const alternativeProfile = {
-      img: ProfileImg2, // Change to another image source
-      name: "IzumiDaichi",
-      role: "LEARNING HOW TO DRAW ANATOMY",
-      description: "Terrible Artist",
-    };
-  
-    // Handle image click to toggle content
-    const handleProfileClick = () => {
-      setProfile((prevProfile) =>
-        prevProfile.name === "Jake Rupisan" ? alternativeProfile : {
-          img: ProfileImg,
-          name: "Jake Rupisan",
-          role: "TRAINING FOR UI & UX, FRONTEND",
-          description: "Student"
-        }
-      );
-    };
+  const [bgColor, setBgColor] = useState("bg-gradient-to-b from-[#E5F9F3] via-[#CCEBF0] to-[#B5D7E8] dark:bg-gradient-to-b dark:from-[rgb(19,132,98)] dark:via-[#2B8998] dark:to-[#184860] "); // Default background color
+
+  // Define alternative profile content and background color
+  const alternativeProfile = {
+    img: ProfileImg2, // Change to another image source
+    name: "IzumiDaichi",
+    role: "LEARNING HOW TO DRAW ANATOMY",
+    description: "Novice Artist",
+  };
+
+  const alternativeBgColor = "bg-gradient-to-b from-[#A2E5E1] via-[#99D3D6] to-[#7AB2C6] dark:bg-gradient-to-b dark:from-[rgb(13, 92, 70)] dark:via-[#1F6B7A] dark:to-[#0F3B48]"; // New background color for the alternative profile
+
+  // Handle image click to toggle content and background color
+  const handleProfileClick = () => {
+    if (profile.name === "Jake Rupisan") {
+      setProfile(alternativeProfile);
+      setBgColor(alternativeBgColor);
+    } else {
+      setProfile({
+        img: ProfileImg,
+        name: "Jake Rupisan",
+        role: "TRAINING FOR WEB DEVELOPMENT",
+        description: "Student",
+      });
+      setBgColor("bg-gradient-to-b from-[#E5F9F3] via-[#CCEBF0] to-[#B5D7E8] dark:bg-gradient-to-b dark:from-[rgb(19,132,98)] dark:via-[#2B8998] dark:to-[#184860] "); // Reset to default color
+    }
+  };
+
   return (
-    <div
-        className="flex flex-row justify-center"
-  >
-    <div     onClick={handleProfileClick}>
-    <motion.div
-    whileTap={{  scale: 0.95, rotateY: 90}}
-    variants={fadeIn("fade", 0.9)}
-    initial="hidden"
-    whileInView={"show"}
-    viewport={{ once: false, amount: 0.5 }}
-    className="bg-gradient-to-b from-[#E5F9F3] via-[#CCEBF0] to-[#B5D7E8] dark:bg-gradient-to-b dark:from-[rgb(19,132,98)] dark:via-[#2B8998] dark:to-[#184860] 
-    rounded-2xl p-11 shadow-md max-w-screen-full cursor-pointer">
-      <motion.div
-        variants={fadeIn("down", 1)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0 }}
-        className="flex flex-col items-center justify-center"
-      >
-        {/* Image that changes on click */}
-        <Image
-          src={profile.img}
-          alt={profile.name}
-          className="rounded-full object-cover w-48 h-48 md:w-72 md:h-72 mb-4" 
-        />
-        
-        {/* Text content that changes with the image */}
-        <h2 className="text-xl font-bold text-black">{profile.name}</h2>
-        <p className="text-black font-medium">{profile.role}</p>
-        <p className="text-black font-medium">{profile.description}</p>
+    <div className="flex flex-row justify-center">
+      <div onClick={handleProfileClick}>
+        <motion.div
+          whileTap={{ scale: 0.95, rotateY: 90 }}
+          variants={fadeIn("fade", 0.9)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.5 }}
+          className={`${bgColor} rounded-2xl p-11 shadow-md max-w-screen-full cursor-pointer`}>
+          <motion.div
+            variants={fadeIn("down", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0 }}
+            className="flex flex-col items-center justify-center"
+          >
+            {/* Image that changes on click */}
+            <Image
+              src={profile.img}
+              alt={profile.name}
+              className="rounded-full object-cover w-48 h-48 md:w-72 md:h-72 mb-4"
+            />
 
-        <div className="flex space-x-4">
-          <Socials />
-        </div>
-      </motion.div>
-    </motion.div>
+            {/* Text content that changes with the image */}
+            <h2 className="text-xl font-bold text-black">{profile.name}</h2>
+            <p className="text-black font-medium">{profile.role}</p>
+            <p className="text-black font-medium">{profile.description}</p>
+
+            <div className="flex space-x-4">
+              <Socials />
+            </div>
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default ProfileCard;
--- src/components/contactComponents/form.jsx
+++ src/components/contactComponents/form.jsx
@@ -1,116 +1,162 @@
-"use client"
-import React from 'react'
-import { useForm, ValidationError } from '@formspree/react';
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react'; 
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-  } from "@/components/ui/card";
-  import { FaStar } from "react-icons/fa6";
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { FaStar } from "react-icons/fa6";
 import { motion } from "framer-motion";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { RiMailSendFill } from "react-icons/ri";
 
+function FormComponent() {
+  const [state, handleSubmit] = useForm("mrbgzvrb"); // Replace with your Formspree ID
+  const [showError, setShowError] = React.useState(false);
 
+  React.useEffect(() => {
+    // Check if there are errors and set showError state accordingly
+    if (state.errors && state.errors.length > 0) {
+      setShowError(true);
+    }
+  }, [state.errors]);
 
-
-function form() {
-
-    const [state, handleSubmit] = useForm("mrbgzvrb");
   if (state.succeeded) {
-      return (
-        <Alert>
-        <RiMailSendFill className="fill-[#0C9C3D] h-4 w-4" />
-        <AlertTitle>SUCCESS!</AlertTitle>
-        <AlertDescription className='text-[#0C9C3D]'>
-          Your Message has been Sent.
-        </AlertDescription>
-      </Alert>
-      );
-      
+    return (
+      <motion.div
+        id='MformContainer'
+        className="flex justify-center items-center h-screen"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Alert id='successform' className="bg-white shadow-lg rounded-xl p-6 w-96 text-center">
+          <motion.div
+            className="flex justify-center mb-4"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1.5 }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
+          >
+            <RiMailSendFill className="text-[#0C9C3D] h-16 w-16" />
+          </motion.div>
+          <AlertTitle className="text-2xl font-bold text-[#0C9C3D]">
+            Success!
+          </AlertTitle>
+          <AlertDescription className="text-gray-700 mt-2 text-lg">
+            Your message has been successfully sent.
+          </AlertDescription>
+          <AlertDescription className="text-gray-500 mt-2 text-sm">
+            I'll get back to you soon. Thank you for reaching out!
+          </AlertDescription>
+        </Alert>
+      </motion.div>
+    );
   }
-
 
   return (
     <>
-    <div className='flex flex-row justify-center'>
-    <Card className="rounded-2xl m-6 pt-2 bg-[#1F2937]">
-        <FaStar className="size-10 shadow-none fill-[#FFB955] animate-pulse m-3 flex justify-start" />
+      <div className="flex flex-row justify-center">
+        <Card className="rounded-2xl m-6 pt-2 bg-[#1F2937]">
+          <FaStar className="size-10 shadow-none fill-[#FFB955] animate-pulse m-3 flex justify-start" />
+          <CardHeader>
+            <CardTitle className="text-center text-white font-bold">
+              Let's Work Together!
+            </CardTitle>
+          </CardHeader>
+          <div className="mx-3">
+            <CardDescription className="text-center text-white">
+              If you are interested in working together with me
+            </CardDescription>
+            <CardDescription className="text-center text-white">
+              Get in touch by completing the form below or by emailing me at
+            </CardDescription>
+            <CardDescription className="text-center text-1xl font-medium mt-2 text-white">
+              JakeRupisan2016@gmail.com
+            </CardDescription>
+          </div>
 
-        <CardHeader>
-        <CardTitle className="text-center text-white font-bold"> Let's Work Together! </CardTitle>
-        </CardHeader>
+          <CardContent>
+            <form
+              className="flex justify-center flex-col mb-12 mt-3"
+              onSubmit={handleSubmit} // Attach Formspree handleSubmit
+            >
+              <label className="labels pb-2 text-white" htmlFor="name">
+                Name
+              </label>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
+                required
+              />
+              <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-        <div className="mx-3">
-        <CardDescription className="text-center text-white"> if you are interested on working together with me </CardDescription>
-        <CardDescription className="text-center text-white">  Get in touch by completing the form below or by emailing me at </CardDescription>
-        <CardDescription className="text-center text-1xl font-medium mt-2 text-white"> JakeRupisan2016@gmail.com  </CardDescription>
-        </div>
+              <label className="labels pb-2 text-white" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
+                required
+              />
+              <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-<CardContent>
-     <form className='flex justify-center flex-col mb-12 mt-3' onSubmit={handleSubmit}>
+              <label className="labels pb-2 text-white" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message"
+                className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
+                required
+              />
+              <ValidationError prefix="Message" field="message" errors={state.errors} />
 
-     <label className='labels pb-2 text-white' htmlFor="name"> Name </label>
-      <input
-        id="name"
-        type="name" 
-        name="name"
-        placeholder="Name"
-        className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
-        required
-      />
-      <ValidationError 
-        prefix="Name" 
-        field="name"
-        errors={state.errors}/>
+              <motion.div whileHover={{ scale: 1.1 }} className="flex place-self-end">
+                <button
+                  type="submit"
+                  className="Button px-8 p-3 rounded-xl hover:cursor-pointer hover:bg-[#0C9C3D] hover:shadow-lg hover:shadow-[#006A43] font-medium text-white"
+                  disabled={state.submitting}
+                >
+                  {state.submitting ? "Submitting..." : "Submit"}
+                </button>
+              </motion.div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
 
-      <label className='labels pb-2 text-white' htmlFor="email"> Email Address </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        placeholder="Email"
-        className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
-        required
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-
-<label className='labels pb-2 text-white' htmlFor="email"> Message </label>
-      <textarea
-        id="message"
-        name="message"
-        placeholder="Message"
-        className="rounded-xl text-md mb-4 py-2 pr-18 w-full pl-4"
-        required
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      
-      <motion.div whileHover={{ scale: 1.1 }} className="flex place-self-end">
-      <button 
-      type="submit" 
-      className="Button px-8 p-3 rounded-xl hover:cursor-pointer hover:bg-[#0C9C3D] hover:shadow-lg hover:shadow-[#006A43] font-medium text-white" 
-      disabled={state.submitting}>
-        Submit
-      </button>
-      </motion.div>
-
-    </form>
-    </CardContent>
-    </Card>
-    </div>
-
+      {/* Error Pop-up Modal */}
+      {showError && (
+        <motion.div
+          className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <div className="bg-white rounded-xl p-6 shadow-xl w-80 text-center">
+            <AlertTitle className="text-xl font-bold text-red-600">Submission Failed!</AlertTitle>
+            <AlertDescription className="text-gray-700 mt-4">
+              There was an error submitting your message. Please try again later.
+            </AlertDescription>
+            <button
+              className="mt-6 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700"
+              onClick={() => setShowError(false)} // Close the modal
+            >
+              Close
+            </button>
+          </div>
+        </motion.div>
+      )}
     </>
-  )
+  );
 }
 
-export default form
+export default FormComponent;
