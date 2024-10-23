16c7bc4b92c2e10eaa7250d7c9c3436acb52ae15
Izumi <izumidaichi2016@gmail.com>
2024-10-23 10:43:00 +0800

    Revert "Update page.js"
    This reverts commit 970e2356c9bc90bfa4268267e5ba1d1a53f4709f.

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import ContactForm from '@/components/contactComponents/form';
-import Image from 'next/image';
+import React from 'react'
+import ContactForm from '@/components/contactComponents/form'
+import Image from 'next/image'
 import ContactAvatar from "@/Images/ContactAvatar.png";
+
 
 function page() {
   return (
-    <div
-      id="contact"
-      className="container h-dvh bg-[#253558] rounded-lg shadow-lg flex flex-col overflow-hidden"
-    >
-      <h1 className="text-4xl text-white font-semibold my-4">/Contact</h1>
+    <div  id="contact"
+      className="container h-dvh bg-[#253558] rounded-lg shadow-lg flex flex-col overflow-hidden">
+        <h1 className='text-4xl text-white font-semibold my-4' >/Contact</h1>
 
-      <div id="cardform" className="grid grid-cols-2 bg-[#253558]">
-        <ContactForm />
+        <div id='cardform' className='grid grid-cols-2 bg-[#253558]'>
+          
+            <ContactForm/>
+      
+            <Image
+            id="contactimg"
+            alt="ContactAvatar"
+            className="floating flex self-center justify-self-center pr-3"
+            src={ContactAvatar}>
+          </Image>
 
-        <Image
-          id="contactimg"
-          alt="Contact Avatar"
-          className="floating flex self-center justify-self-center pr-3"
-          src={ContactAvatar}
-          width={400} // Adjust width and height accordingly
-          height={400} // Adjust width and height accordingly
-        />
+          </div>
       </div>
-    </div>
-  );
+  )
 }
 
-export default page;
+export default page
