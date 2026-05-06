2ed534433eae69a60e7473a78cdb35c705cae6a5
Jake P. Rupisan <161425488+IzumiDaichi@users.noreply.github.com>
2026-05-07 05:17:33 +0800

    add projects id

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -2,7 +2,7 @@ import ProjectTabs from "@/components/sections/ProjectTabs";
 
 export default function Projects() {
   return (
-    <div className="container mt-16">
+    <div id="projects" className="container mt-16">
       <h1 className="mb-6">Projects</h1>
       <ProjectTabs />
     </div>
