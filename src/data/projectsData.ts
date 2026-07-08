bfd8910f3d96253b7bf9ff80c80c07b170bb7f5f
Jake P. Rupisan <161425488+IzumiDaichi@users.noreply.github.com>
2026-07-08 23:25:47 +0800

    add project
    -fastbreak tracker

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -15,7 +15,7 @@ export interface Project {
 export const projects: Project[] = [
   {
     category: "Code",
-    images: ["/Portfolio.webp","/Portfolio.png","/Portfolio1.png","/Portfolio2.png","/Portfolio3.png","/Portfolio4.png","/Portfolio5.png"],
+    images: ["/Portfolio.webp", "/Portfolio.png", "/Portfolio1.png", "/Portfolio2.png", "/Portfolio3.png", "/Portfolio4.png", "/Portfolio5.png"],
     title: "Portfolio v1",
     description:
       "The first portfolio I made and my first task during my internship in Techstacks. I planned the early design using Figma, added a custom intro loading animation, and used my drawings for several components.",
@@ -34,26 +34,6 @@ export const projects: Project[] = [
     status: "live",
   },
   {
-    category: "UI & UX",
-    images: ["/ElsFigma.png","/ElsFigma1.png","/ElsFigma2.png","/ElsFigma3.png","/ElsFigma4.webp","/ElsFigma5.png","/ElsFigma6.png","/ElsFigma7.png"],
-    title: "Elsword website copy",
-    description:
-      "A copy of the ELSWORD website I made in Figma to practice my UI & UX design skills. Only the home page is complete. I plan to finish the rest with a fully functional prototype view.",
-    tags: ["Figma"],
-    link: "https://www.figma.com/design/gR7fKr5psqe506t8eRuq1L/Copy-of-ELSWORD-website?node-id=0-1&t=kMpvZXES1EhGktvV-1",
-    status: "wip",
-  },
-  {
-    category: "UI & UX",
-    images: ["/KRMerch.png","/KRMerch1.png","/KRMerch2.png","/KRMerch3.png","/KRMerch4.png","/KRMerch5.png"],
-    title: "Korean Merchandise Online Store",
-    description:
-      "A design for an online store selling Korean merchandise, such as K-pop albums, lightsticks, and fan-made goods. This is a Prototype of the website that me and my groupmates made for our capstone project in college. We used Figma to design the UI and create a clickable prototype.",
-    tags: ["Figma"],
-    link: "https://www.figma.com/design/mZao40z4eofg9vxQL9vX0h/GA69-Korean-Merchandise?node-id=0-1&t=0j3LU1JBlT7MgjeU-1",
-    status: "collab",
-  },
-  {
     category: "Code",
     images: ["/FBClone.webp"],
     title: "Facebook Clone",
@@ -64,8 +44,42 @@ export const projects: Project[] = [
     status: "collab",
   },
   {
+    category: "Code",
+    images: ["/Fastbreak.webp",],
+    title: "Fastbreak Transaction Tracker",
+    description:
+      "The first project i made myself with the help of CLAUDE. This desktop application is designed to simplify storage business management. Track buying and selling transactions, monitor capital, analyze profits, and manage multiple storage terms (seasons or years). Generate detailed reports and keep your data securely synced across devices with real-time cloud synchronization.",
+    tags: [
+      "Next.js",
+      "Electron",
+      "Google Cloud API",
+    ],
+    status: "wip",
+  },
+  {
+    category: "UI & UX",
+    images: ["/ElsFigma.png", "/ElsFigma1.png", "/ElsFigma2.png", "/ElsFigma3.png", "/ElsFigma4.webp", "/ElsFigma5.png", "/ElsFigma6.png", "/ElsFigma7.png"],
+    title: "Elsword website copy",
+    description:
+      "A copy of the ELSWORD website I made in Figma to practice my UI & UX design skills. Only the home page is complete. I plan to finish the rest with a fully functional prototype view.",
+    tags: ["Figma"],
+    link: "https://www.figma.com/design/gR7fKr5psqe506t8eRuq1L/Copy-of-ELSWORD-website?node-id=0-1&t=kMpvZXES1EhGktvV-1",
+    status: "wip",
+  },
+  {
+    category: "UI & UX",
+    images: ["/KRMerch.png", "/KRMerch1.png", "/KRMerch2.png", "/KRMerch3.png", "/KRMerch4.png", "/KRMerch5.png"],
+    title: "Korean Merchandise Online Store",
+    description:
+      "A design for an online store selling Korean merchandise, such as K-pop albums, lightsticks, and fan-made goods. This is a Prototype of the website that me and my groupmates made for our capstone project in college. We used Figma to design the UI and create a clickable prototype.",
+    tags: ["Figma"],
+    link: "https://www.figma.com/design/mZao40z4eofg9vxQL9vX0h/GA69-Korean-Merchandise?node-id=0-1&t=0j3LU1JBlT7MgjeU-1",
+    status: "collab",
+  },
+
+  {
     category: "Art",
-    images: ["/Marketing.jpg","/Marketing1.webp","/Marketing2.webp","/Marketing3.webp","/Marketing4.webp"],
+    images: ["/Marketing.jpg", "/Marketing1.webp", "/Marketing2.webp", "/Marketing3.webp", "/Marketing4.webp"],
     title: "Marketing Graphic Designs",
     description:
       "A collection of graphic designs I made for marketing purposes, such as social media posts, banners, and promotional materials.",
